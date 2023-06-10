e5627ba32ffb1e81a9fb999bc517166d43e9f174
Daniel Jimenez <daniel@djimenezdev.com>
2023-06-10 02:32:00 -0700

    physics and 3d config

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
+import { Suspense } from "react";
+import { Physics } from "@react-three/rapier";
 
 function App() {
   return (
-    <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
+    <Canvas shadows camera={{ position: [3, 3, 5], fov: 45 }}>
       <color attach="background" args={["#ececec"]} />
-      <Experience />
+      <Suspense>
+        <Physics debug>
+          <Experience />
+        </Physics>
+      </Suspense>
     </Canvas>
   );
 }
